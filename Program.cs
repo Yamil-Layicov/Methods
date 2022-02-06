c17cb429d65ecbce65017c67c905d0924884186b
Yamil-Layicov <yamilbl@code.edu.az>
2022-02-07 00:26:04 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sozunuzu daxil edin");
+            string temp_word = Console.ReadLine();    
+
+
+            Console.WriteLine("Herf Daxil Edin");
+            char c;
+            char.TryParse(Console.ReadLine(), out c);
+
+
+            Console.WriteLine(FindCharIndex(temp_word, c));
+
+        }
+        static int FindCharIndex(string temp_word, char c)
+        {
+            char[] charString = temp_word.ToCharArray();
+            for (int i = 0; i < temp_word.Length; i++)
+            {
+
+                if (c == charString[i])
+                {
+
+                    return i;
+                }
+
+            }
+            return -1;
+        }
+    }
+}
